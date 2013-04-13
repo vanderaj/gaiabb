855952e68aac30131f42704e8fc972c50225a31d
vanderaj@gmail.com <vanderaj@gmail.com@1b21442c-caa8-11de-8f3f-2f730f80caef>
2013-04-13 13:36:04 +0000

    Update copyright to 2013

--- source/header.php
+++ source/header.php
@@ -909,11 +909,11 @@ if (!isset($CONFIG['max_reg_day']) || $CONFIG['max_reg_day'] < 1 || $CONFIG['max
 // display version build (John)
 if ($CONFIG['show_full_info'] == 'on')
 {
-    $versionlong = '<br />Powered by <a href="http://www.GaiaBB.com" target="_blank"><strong>'.$versionshort.'</strong></a> ('.$alpha.''.$beta.''.$gamma.''.$sp.'), &copy; 2011 The GaiaBB Group';
+    $versionlong = '<br />Powered by <a href="http://www.GaiaBB.com" target="_blank"><strong>'.$versionshort.'</strong></a> ('.$alpha.''.$beta.''.$gamma.''.$sp.'), &copy; 2011-2013 The GaiaBB Group';
 }
 else
 {
-    $versionlong = '<br />Powered by <a href="http://www.GaiaBB.com" target="_blank"><strong>'.$versionshort.'</strong></a>, &copy; 2011 The GaiaBB Group';
+    $versionlong = '<br />Powered by <a href="http://www.GaiaBB.com" target="_blank"><strong>'.$versionshort.'</strong></a>, &copy; 2011-2013 The GaiaBB Group';
 }
 
 // If the board is offline, display an appropriate message
