43600b6c809ef4c6c2b1e45703821e22ee6ed2cb
Andrew van der Stock <andrew.vanderstock@owasp.com>
2021-01-22 08:46:58 -0700

    Start to add end to end tests
    Robot Framework installation is required.

---
+++ tests/activity.robot
@@ -0,0 +1,15 @@
+*** Settings ***
+Documentation Tests for the activity.php 
+Library SeleniumLibrary
+
+*** Variables ***
+${URL}      http://forums.aussieveedubbers.local:8080/activity.php
+${BROWSER}  Chrome
+
+*** Test Cases ***
+Display default activity
+
+Display sort orders
+
+
+
---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'dd6771e64dcb87b2d0f181eedf739945483e7874',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'dd6771e64dcb87b2d0f181eedf739945483e7874',
+    ),
+    'squizlabs/php_codesniffer' => 
+    array (
+      'pretty_version' => '3.5.8',
+      'version' => '3.5.8.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '9d583721a7157ee997f235f327de038e7ea6dac4',
+    ),
+  ),
+);
